93df2225b0efd79437c75611b802c62eaf14fac8
Henry Brophy <henrybrophy93@gmail.com>
2020-12-08 08:49:12 -0500

    Done for the night.

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -13,6 +13,39 @@ Notes:
 - You may not use the same element twice.
 - You can return the answer in any order.
 """
-def two_sum(nums, target):
-    # Your code here
 
+
+def two_sum(nums, target):
+    # Slow Solution O(n^2)
+    # for i in range(len(nums)):
+    #     for j in range(i+1, len(nums)):
+    #         num1 = nums[i]
+    #         num2 = nums[j]
+    #         if num1 + num2 == target:
+    #             return [i, j]
+    # return None
+
+    # Fast Solution O(N)
+    num_dict = {}
+
+    for i in range(len(nums)):
+        num_dict[nums[i]] = i
+
+    for i in range(len(nums)):
+        current_num = nums[i]
+        complement = target - current_num
+        if complement in num_dict and i != num_dict[complement]:
+            return [i, num_dict[complement]]
+
+
+print(two_sum(nums=[2, 5, 9, 13], target=7))
+print(two_sum(nums=[4, 3, 5], target=8))
+
+# Your code here
+# Input =  list of number and a integer
+# Output = indices of a list that add up to the target return None if no answer.
+
+# Plan
+
+# Start with one of the pointers aat arr[0] and another one at arr[1] see
+# if they add up to the target and increment until they do
